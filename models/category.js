1f4d91254f119ac442b3130a923e07738c7e5bf9
spracious <110341045+spracious@users.noreply.github.com>
2023-09-24 17:51:38 +0100

    added the current changes to the category file.

--- models/category.js
+++ models/category.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema({
-<<<<<<< Updated upstream
   name: {
     type: String,
     enum: ["VIP", "VVIP", "REGULAR"],
@@ -17,22 +16,6 @@ const CategorySchema = new mongoose.Schema({
     required: true,
     ref: "cinemas",
   },
-=======
-    name: {
-      regular: { type: String },
-      vip: { type: String },
-      vvip: { type: String }
-    },
-    price: {
-      type: String,
-      required: true
-    },
-    cinema_id: {
-      type: String,
-      required: true,
-      ref: "cinemas"
-    }
->>>>>>> Stashed changes
 });
 const Category = mongoose.model("categories", CategorySchema);
 module.exports = Category;
