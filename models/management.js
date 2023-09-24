ab4e944f2e3d524bab0c2b1f114dbc32443f4550
Sumerville <uwaaustin10@gmail.com>
2023-09-24 10:17:00 -0700

    added booking and management schema

---
+++ models/management.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const ManagementSchema = new mongoose.Schema({
+    branch_id: {
+        type: String,
+    },
+    fullname: {
+        type: String,
+        required:true
+    },
+    role: {
+        type: String,
+        enum: ["COUNTER", "THEATER_ADMIN", "WEBSITE_ADMIN"],
+        uppercase:true
+    },
+    password: {
+        type: String
+    },
+
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+const Management = mongoose.model("managements", ManagementSchema);
+module.exports = Management 
