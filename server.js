064c892b6d5ca2e7fed4722b991a58326395c4f7
spracious <110341045+spracious@users.noreply.github.com>
2023-09-25 13:59:16 +0100

    updated server.js with....  app.use(express.json()); app.use(express.urlencoded({ extended: true }));

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 let mongoose = require("mongoose");
 let express = require("express");
 let app = express();
-require('dotenv').config()
+require("dotenv").config();
 
 let cinema = require("./routes/cinema");
 
 let PORT = process.env.PORT;
 let MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect(MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true});
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connection.on('open',()=>console.log("Mongo Server connected"));
-mongoose.connection.on('error',(err)=>console.log(err.message));
+mongoose.connection.on("open", () => console.log("Mongo Server connected"));
+mongoose.connection.on("error", (err) => console.log(err.message));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/", cinema);
 
 app.listen(PORT);
-console.log("App runnning on port:"+PORT)
+console.log("App runnning on port:" + PORT);
