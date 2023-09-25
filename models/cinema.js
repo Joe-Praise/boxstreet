4be20c313ed7ecc742798956490a0f2db8c814ed
spracious <110341045+spracious@users.noreply.github.com>
2023-09-25 15:23:47 +0100

    cinema update

--- models/cinema.js
+++ models/cinema.js
@@ -2,11 +2,26 @@ const mongoose = require("mongoose");
 
 const CinemaSchema = new mongoose.Schema({
 
-  name: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
-  image: { type: String }, 
-  date_created: { type: Date, default: Date.now }
+  name: { 
+    type: String,
+    required: true, 
+    unique: true 
+},
+  email: {
+     type: String,
+     required: true,
+     unique: true 
+    },
+  phone: {
+     type: String,
+     required: true 
+    },
+  image: { 
+    type: String 
+}, 
+  date_created: { 
+  type: Date, 
+  default: Date.now }
 })
 
 
