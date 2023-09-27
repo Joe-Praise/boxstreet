e1122bc4ee7647ce3d189fc0760afbdae61169f6
spracious <110341045+spracious@users.noreply.github.com>
2023-09-27 21:25:46 +0100

    cloudinary update

--- routes/user.js
+++ routes/user.js
@@ -87,7 +87,7 @@ app.delete("/:id", Protect, async (req, res) => {
       res.status(200).json({ msg: "User successfully deleted" });
     }
   } catch (err) {
-    res.status(500).json({ err: err.message });
+    res.status(500).json({ err: err.message }); 
   }
 });
 
