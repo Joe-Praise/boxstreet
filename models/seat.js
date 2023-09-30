4898af2afb10d5480c1c1fc7cc5d6b05a5e44f29
Joe Praise <alimipraisejoe@gmail.com>
2023-09-30 23:43:05 +0100

    seats update

--- models/seat.js
+++ models/seat.js
@@ -13,7 +13,14 @@ const SeatSchema = new mongoose.Schema({
   },
   seat_number: { type: String, required: true },
   is_booked: { type: Boolean, default: false },
+  is_deleted: { type: Boolean, default: false, select: false },
   active: { type: Boolean },
+});
+
+SeatSchema.pre(/^find/, function (next) {
+  // hide users with active field set to false
+  this.find({ is_booked: { $ne: false } });
+  next();
 });
 
 const Seat = mongoose.model("seats", SeatSchema);
