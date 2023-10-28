9b5e244f7116c08e63e4175ca6ca0272fa1f3d5a
Sumerville <uwaaustin10@gmail.com>
2023-10-28 03:02:06 -0700

    changes to transactions model

--- routes/transaction.js
+++ routes/transaction.js
@@ -98,11 +98,7 @@ app.get("/summary", async (req, res) => {
   let {branch_id} = req.query;
   try {
     const summary = await Transaction.aggregate([
-      {
-        $match:{
-          branch_id
-        }
-      },
+      
       {
         $lookup: {
           from: "cinemas",
@@ -112,15 +108,15 @@ app.get("/summary", async (req, res) => {
         },
       },
       { $unwind: "$cinema" },
-      // {
-      //   $lookup: {
-      //     from: "branches",
-      //     localField: "branch_id",
-      //     foreignField: "_id",
-      //     as: "branch",
-      //   },
-      // },
-      // { $unwind: "$branch" },
+      {
+        $lookup: {
+          from: "branches",
+          localField: "branch_id",
+          foreignField: "_id",
+          as: "branch",
+        },
+      },
+      { $unwind: "$branch" },
       {
         $group: {
           _id: {
@@ -128,7 +124,7 @@ app.get("/summary", async (req, res) => {
             month: { $month: "$paidAt" },
             day: { $dayOfMonth: "$paidAt" },
             cinema_id: "$cinema",
-            // branch_id: "$branch",
+            branch_id: "$branch",
             $year: {
               $dateFromString: {
                 dateString: "$date",
